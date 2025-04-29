24c1e3c745e98a5656e2f2297020e3973f37603e
maximelabonde <maxime.labonde@protonmail.com>
2025-04-29 14:15:05 +0200

    Update Formspree endpoint in config.js to new URL

--- src/config.js
+++ src/config.js
@@ -126,7 +126,7 @@ export const projectCardImages = [
   Add your formspree endpoint below.
   https://formspree.io/
 */
-export const formspreeUrl = "https://formspree.io/f/YourEndpoint";
+export const formspreeUrl = "https://formspree.io/f/xovdjegr";
 
 // Footer icons theme (light or dark)
 export const footerTheme = "dark";
